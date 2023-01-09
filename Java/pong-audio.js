d90e7d37168926ad25725b0a309256df764637dc
CrochetTessa <121125290+CrochetTessa@users.noreply.github.com>
2023-01-09 12:13:59 -0600

    Update pong-audio.js

--- Java/pong-audio.js
+++ Java/pong-audio.js
@@ -52,12 +52,12 @@ soundArray.push(paddleSound);
 export var scoreSound = new soundFile("tap-percussive.mp3");
 soundArray.push(scoreSound);
 
-export var ambientSound = new soundFile("Spooky Audio.mp3");
+export var ambientSound = new soundFile("");
 soundArray.push(ambientSound);
 ambientSound.player.loop = true; //turn on looping
 ambientSound.player.volume.value = -2; //turn down volume
 
-export var adventureMusic = new soundFile("");
+export var adventureMusic = new soundFile("Spooky Audio.mp3");
 soundArray.push(adventureMusic);
 adventureMusic.player.loop = true;
 adventureMusic.player.volume.value = -16;
